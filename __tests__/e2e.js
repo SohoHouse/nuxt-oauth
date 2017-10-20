3c1c69725fc2b075439fa8e8eb7e29fd5a765b6d
Sam Garson <samtgarson@gmail.com>
2017-10-20 17:34:06 +0100

    e2e test

---
+++ __tests__/e2e.js
@@ -0,0 +1,48 @@
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000
+process.env.PORT = process.env.PORT || 5060
+process.env.NODE_ENV = 'production'
+
+const { Nuxt, Builder } = require('nuxt')
+const request = require('request-promise-native')
+
+const config = require('./fixture/nuxt.config')
+
+const url = path => `http://localhost:${process.env.PORT}${path}`
+const get = path => request({
+  followRedirect: req => req.headers.location.startsWith('/'),
+  resolveWithFullResponse: true,
+  simple: false,
+  url: url(path)
+})
+
+let nuxt
+
+const setup = (configOverride = {}) => {
+  beforeAll(async () => {
+    nuxt = new Nuxt({
+      ...config,
+      ...configOverride
+    })
+    await new Builder(nuxt).build()
+    await nuxt.listen(process.env.PORT)
+  })
+
+  afterAll(async () => {
+    await nuxt.close()
+  })
+}
+
+describe('Prod', () => {
+  setup()
+
+  test('render', async () => {
+    const { body } = await get('/')
+    expect(body).toContain('Works!')
+  })
+
+  test('secret redirect', async () => {
+    const { statusCode, headers } = await get('/secret')
+    expect(statusCode).toEqual(302)
+    expect(headers.location).toContain(config.oauth.oauthHost)
+  })
+})
---
+++ __tests__/fixture/pages/secret.vue
@@ -0,0 +1,11 @@
+<template>
+<div>
+    Shhh...
+</div>
+</template>
+
+<script>
+export default {
+  authenticated: true
+}
+</script>
