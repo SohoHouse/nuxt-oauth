0103ec2c121094bfcc7f99faee41aebbd901d2e9
Matthew <matthewhadwen@gmail.com>
2022-10-05 10:54:58 +0100

    Add inline code documentation using JS Doc Blocks

--- index.js
+++ index.js
@@ -11,8 +11,11 @@ const defaultOptions = {
 }
 
 module.exports = function NuxtOAuth (moduleOptions) {
-  const options = Object.assign(defaultOptions, moduleOptions, this.options.oauth)
+  // NOTE: moduleOptions should be last according to the Nuxt documentation
+  // @see https://nuxtjs.org/docs/directory-structure/modules/
+  const options = Object.assign({}, defaultOptions, moduleOptions, this.options.oauth)
 
+  // Check for required options types
   if (typeof options.onLogout !== 'function') throw new Error('options.onLogout must be a function')
   if (typeof options.fetchUser !== 'function') throw new Error('options.fetchUser must be a function')
   if (options.scopes && !Array.isArray(options.scopes)) throw new Error('options.scopes must be an array')
@@ -32,7 +35,7 @@ module.exports = function NuxtOAuth (moduleOptions) {
   this.options.router.middleware = this.options.router.middleware || []
   this.options.router.middleware.push('auth')
 
-  // Setup te /auth/login route
+  // Setup the /auth/login route
   this.extendRoutes((routes, res) => {
     routes.push({
       name: 'oauth-login',
