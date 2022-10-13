e4b7f7d97624a84f9504b9fa4b34a3e7aa3f72ad
Matthew <matthewhadwen@gmail.com>
2022-10-13 23:19:39 +0100

    Refactor nuxt-oauth handler

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { resolve } = require('path')
-const base = require('./lib/server-middleware')
+
+const ServerMiddleware = require('./lib/server-middleware')
+
+const { DEFAULT_SESSION_DURATION } = require('./lib/constants')
 
 const defaultOptions = {
   moduleName: 'oauth',
@@ -7,26 +10,31 @@ const defaultOptions = {
   onLogout: () => {},
   scopes: [],
   pageComponentPath: resolve(__dirname, './lib/route.js'),
-  sessionDuration: 24 * 60 * 60 * 1000 // 24 hours
+  sessionDuration: DEFAULT_SESSION_DURATION,
+  protocol: (req) => req.protocol,
+  host: (req) => req.headers.host,
+  logging: true,
 }
 
 module.exports = function NuxtOAuth (moduleOptions) {
-  // NOTE: moduleOptions should be last according to the Nuxt documentation
-  // @see https://nuxtjs.org/docs/directory-structure/modules/
-  const options = Object.assign({}, defaultOptions, moduleOptions, this.options.oauth)
-
+  const options = Object.assign({}, defaultOptions, this.options.oauth, moduleOptions)
+  
   // Check for required options types
   if (typeof options.onLogout !== 'function') throw new Error('options.onLogout must be a function')
   if (typeof options.fetchUser !== 'function') throw new Error('options.fetchUser must be a function')
   if (options.scopes && !Array.isArray(options.scopes)) throw new Error('options.scopes must be an array')
-
+  
   // Setup middlewares
-  this.options.serverMiddleware.unshift(base(options))
+  const OAuthMiddleware = ServerMiddleware(options)
+
+  this.options.serverMiddleware.unshift(OAuthMiddleware)
+  
   this.addPlugin({
     src: resolve(__dirname, 'lib/plugin.js'),
     fileName: 'nuxt-oauth.plugin.js',
     options: {
-      moduleName: options.moduleName
+      moduleName: options.moduleName,
+      sessionName: options.sessionName,
     }
   })
 
@@ -49,4 +57,5 @@ module.exports = function NuxtOAuth (moduleOptions) {
   })
 }
 
+// @see https://nuxtjs.org/docs/directory-structure/modules#write-your-own-module
 module.exports.meta = require('./package.json')
--- test/e2e/fixture/pages/index.vue
+++ test/e2e/fixture/pages/index.vue
@@ -2,6 +2,7 @@
 <div>
     <h1>Works!</h1>
     <a href="/test">Test something</a>
+    <a href="/secret">Secret Page</a>
     <button @click="$login()">Login</button>
 </div>
 </template>
